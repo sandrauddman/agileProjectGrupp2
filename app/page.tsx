600382f74cc0256403207c6891a33fe9ce41d385
dame9785 <soderbergdavid97@gmail.com>
2026-08-14 07:21:56 +0200

    Added props for categories in product modal

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home({ searchParams }: PageProps) {
 
   return (
     <main className="min-h-screen bg-gray-50">
-      <Header />
+      <Header categories={categories} />
 
       <div className="container max-w-7xl mx-auto px-6 py-6">
         <InventoryStatistics />

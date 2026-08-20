e80093c3ed92f07a8abdd1b65d37dd5012e7e219
sandrauddman <sandra.uddman@gmail.com>
2026-08-20 16:54:36 +0200

    added lost changes

--- services/product-service.ts
+++ services/product-service.ts
@@ -8,10 +8,17 @@ export default class ProductService {
   static async getProducts(currentPage: number, categoryParams: string, stockParams: string): Promise<ApiResponse<ProductsResponse>> {
     try {
       const category = categoryParams ? `&categoryId=${categoryParams}` : '';
-      const stock = stockParams ?? '';
+      const stockFilters: Record<string, string> = {
+
+        inStock: `&stock_gte=10`,
+        lowStock: `&stock_gte=1&stock_lte=10`,
+        outofStock: `&stock_lte=0`,
+      };
+
+      const stock = stockFilters[stockParams] ?? "";
 
             
-      const response = await fetch(`${API_URL}/products?_page=${currentPage}&_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category${category}${stock}`, {
+      const response = await fetch(`${API_URL}/products?_page=${currentPage}&_limit=${defaultLimit}&_sort=id&_order=desc&_expand=category${category}&_expand=stock${stock}`, {
         method: 'GET',
       });
 

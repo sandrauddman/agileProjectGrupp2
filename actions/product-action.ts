9eacef1f4cc41011c06dd1fd2f552fa3a5608e2c
Kevindotdev <82119594+Kevindotdev@users.noreply.github.com>
2026-08-24 15:51:34 +0200

    feat: add updateProduct server action

--- actions/product-action.ts
+++ actions/product-action.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from 'next/cache';
 import ProductService from '@/services/product-service';
+import type { Product } from '@/app/types';
 
 export async function deleteProduct(productId: number) {
   const response = await ProductService.deleteProduct(productId);
@@ -19,5 +20,24 @@ export async function deleteProduct(productId: number) {
   return {
     success: response.success,
     message: 'Product deleted successfully',
+  };
+}
+
+export async function updateProduct(productId: number, product: Partial<Product>) {
+  const response = await ProductService.updateProduct(productId, product);
+
+  if (!response.success) {
+    return {
+      success: response.success,
+      message: 'Product could not be updated',
+    };
+  }
+
+  // Telling NEXT.JS that product list needs to be updated
+  revalidatePath('/');
+
+  return {
+    success: response.success,
+    message: 'Product updated successfully',
   };
 }

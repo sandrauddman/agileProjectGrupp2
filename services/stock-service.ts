9680b87b3a85e707eb11dcdde535db6d1ccda33d
sandrauddman <sandra.uddman@gmail.com>
2026-08-27 10:31:02 +0200

    fix on functionality and moved type to type.ts

--- services/stock-service.ts
+++ services/stock-service.ts
@@ -1,36 +1,21 @@
-import { ApiResponse, ApiSuccessResponse, ProductsResponse } from "@/app/types";
+import { ApiResponse, ApiSuccessResponse, ProductsResponse, Stats } from "@/app/types";
 import { errorResponse } from "@/utils/error-response";
 
 
-
-
-
-type Stats = {
-    total: number,
-    inStock: number,
-    lowStock: number,
-    outOfStock: number
-}
-
 const API_URL = 'http://localhost:4000';
 
-
-
-
 export default class StockService {
-  
-   
 
-  //GET: Product
+  //GET: allProducts move to product service?
   static async getAllProducts(): Promise<ApiResponse<ProductsResponse>> {
     try {
       const response = await fetch(`${API_URL}/products`, {
         method: 'GET',
       });
 
-       const result = await response.json();
+      const result = await response.json();
 
-       return {
+      return {
         success: true,
         data: result,
       } satisfies ApiSuccessResponse<ProductsResponse>;
@@ -40,28 +25,26 @@ export default class StockService {
   }
 
 
-
-static async getStock(): Promise<Stats> {
+  //Calculates stock and returns stats
+  static async getStock(): Promise<Stats> {
 
     const [productResponse] = await Promise.all([StockService.getAllProducts()]);
 
     const products = productResponse.success ? productResponse.data.products : [];
 
     const currentStats: Stats = {
-        total: products.length,
-        inStock: 0,
-        lowStock: 0,
-        outOfStock: 0
+      total: products.length,
+      inStock: 0,
+      lowStock: 0,
+      outOfStock: 0
     }
     for (const prod of products) {
-        if (prod.stock) {
-            if (prod.stock === 0) currentStats.outOfStock++
-            if (prod.stock >= 10) currentStats.inStock++
-            if (prod.stock > 0 && prod.stock < 10) currentStats.lowStock++
-        }
+      if (prod.stock === 0) currentStats.outOfStock++
+      if (prod.stock! >= 10) currentStats.inStock++
+      if (prod.stock! > 0 && prod.stock! < 10) currentStats.lowStock++
     }
 
     return currentStats;
 
-}
+  }
 }
